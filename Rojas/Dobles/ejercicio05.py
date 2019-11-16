e332552828e9e79adb8b7f714941f50d7e9f41b6
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-11-16 16:09:48 -0500

    se agrego el quinto ejercicio de condicional doble

--- Rojas/Dobles/ejercicio05.py
+++ Rojas/Dobles/ejercicio05.py
@@ -13,14 +13,6 @@ tiempo_vuelo=(2*velocidad_inicial*math.sin(x))/gravedad
 #verificador
 vuelo_corto=(tiempo_vuelo<5)
 
-#condicion doble
-#si el tiempo de vuelo es corto mostrar
-if (vuelo_corto == True):
-    print("                 ESTO SERA RAPIDO!             ")
-else:
-    print(" El tiempo de vuelo no es corto ")
-#FIN_IF
-
 #output
 print("                                                         ")
 print("#########################################################")
@@ -32,5 +24,13 @@ print("# El valor del seno del angulo",y,"es:",math.sin(x))
 print("--------------------------------------------------------#")
 print("# El tiempo de vuelo del proyectil es:",tiempo_vuelo,"s")
 print("#########################################################")
+
+#condicion doble
+#si el tiempo de vuelo es corto mostrar
+if (vuelo_corto == True):
+    print("                 ESTO SERA RAPIDO!             ")
+else:
+    print(" El tiempo de vuelo no es corto ")
+#FIN_IF
 
 
