0373d6d45c341f65141fc0891c357c0ed90cbd9c
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-11-16 14:04:46 -0500

    se agrego el primer ejercicio de condicional multiple

--- Rojas/Multiples/ejercicio01.py
+++ Rojas/Multiples/ejercicio01.py
@@ -11,6 +11,18 @@ velocidad_final = velocidad_inicial+(aceleracion*tiempo)
 #verificador
 alta_velocidad=(velocidad_final > 200)
 
+#condicion multiple
+#si la velocidad es mayor que 200 mostrar advertencia
+if (velocidad_final > 200):
+    print("                VAS COMO UN RAYO!!!              ")
+if (100<velocidad_final<200):
+    print(" Buena velocidad ")
+if (0<velocidad_final<100):
+    print(" La velocidad es muy baja ")
+else:
+    print(" valores erroneos ")
+#FIN_IF
+
 #output
 print("                                                    ")
 print("####################################################")
@@ -24,14 +36,3 @@ print("# Velocidad final del auto :  ", velocidad_final,"km/h")
 print("####################################################")
 
 
-#condicion multiple
-#si la velocidad es mayor que 200 mostrar advertencia
-if (velocidad_final > 200):
-    print("                VAS COMO UN RAYO!!!              ")
-if (100<velocidad_final<200):
-    print(" Buena velocidad ")
-if (0<velocidad_final<100):
-    print(" La velocidad es muy baja ")
-else:
-    print(" valores erroneos ")
-#FIN_IF
