ef2288d6537b9cba3ece26f9101be2eb37580871
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-11-16 15:53:30 -0500

    se agrego el quinto ejercicio de condicional simple

--- Rojas/Simples/ejercicio05.py
+++ Rojas/Simples/ejercicio05.py
@@ -13,12 +13,6 @@ tiempo_vuelo=(2*velocidad_inicial*math.sin(x))/gravedad
 #verificador
 vuelo_corto=(tiempo_vuelo<5)
 
-#condicion simple
-#si el tiempo de vuelo es corto mostrar
-if (vuelo_corto == True):
-    print("                 ESTO SERA RAPIDO!             ")
-#FIN_IF
-
 #output
 print("                                                         ")
 print("#########################################################")
@@ -30,5 +24,11 @@ print("# El valor del seno del angulo",y,"es:",math.sin(x))
 print("--------------------------------------------------------#")
 print("# El tiempo de vuelo del proyectil es:",tiempo_vuelo,"s")
 print("#########################################################")
+
+#condicion simple
+#si el tiempo de vuelo es corto mostrar
+if (vuelo_corto == True):
+    print("                 ESTO SERA RAPIDO!             ")
+#FIN_IF
 
 
