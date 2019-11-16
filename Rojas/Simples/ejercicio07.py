67f1707e450b1d8af93a2bd44f5bda78f609178c
JhordanRojas <56855169+jhordanrojas@users.noreply.github.com>
2019-11-16 14:01:01 -0500

    se agrego el septimo ejercicio de condicional simple

--- Rojas/Simples/ejercicio07.py
+++ Rojas/Simples/ejercicio07.py
@@ -12,6 +12,12 @@ longitud_final=longitud+dilatacion_lineal
 #verificador
 mucha_dilatacion=(dilatacion_lineal>0.5)
 
+#condicion simple
+#si la dilatacion lineal es alta mostrar
+if (mucha_dilatacion == True):
+    print("              LA DILATACION ES EXCESIVA           ")
+#FIN_IF
+
 #output
 print("                                                            ")
 print("############################################################")
@@ -25,9 +31,4 @@ print("# La dilatacion de la varilla es:",dilatacion_lineal,"m")
 print("# La longitud final de la varilla es:",longitud_final,"m")
 print("############################################################")
 
-#condicion simple
-#si la dilatacion lineal es alta mostrar
-if (mucha_dilatacion == True):
-    print("              LA DILATACION ES EXCESIVA           ")
-#FIN_IF
 
